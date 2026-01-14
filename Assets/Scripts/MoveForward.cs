ade97e1f3f9838b8f864a7713a94a80062d0691b
AlexandrOryol <oryolalexandr@gmail.com>
2026-01-14 09:33:12 +0300

    Модификация MoveForward
    Удалил MoveRotateAndGrow
    Модифицировал MoveForward

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -3,9 +3,37 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     public float Speed = 5f;
-    
+
+    public enum MoveDirection { WorldForward, LocalForward, LocalBack, Right, Up };
+    public MoveDirection Direction = MoveDirection.LocalForward;
+
     private void Update()
     {
-        transform.position += transform.forward * Speed * Time.deltaTime;
+        Vector3 moveVector = GetMoveDirection();
+        transform.position += moveVector * Speed * Time.deltaTime;
+    }
+
+    private Vector3 GetMoveDirection()
+    {
+        switch (Direction)
+        {
+            case MoveDirection.WorldForward:
+                return Vector3.forward;
+
+            case MoveDirection.LocalForward:
+                return transform.forward;
+
+            case MoveDirection.LocalBack:
+                return -transform.forward;
+
+            case MoveDirection.Right:
+                return transform.right;
+
+            case MoveDirection.Up:
+                return transform.up;
+
+            default:
+                return transform.forward;
+        }
     }
 }
